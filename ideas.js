18e9cd19e302a22ad9dfbb708d6f8ffb78aa1bae
FabioS <52416951+fabio2100@users.noreply.github.com>
2021-08-03 11:32:30 -0300

    Add files via upload

---
+++ ideas.js
@@ -0,0 +1,43 @@
+new Vue({
+  el: '#app',
+  data: {
+    ideaTxt: '',
+    prioridad: '',
+    listaIdea: [],
+    searchTxt : '',
+    listaUsuarios : []
+  },
+  methods: {
+    agregarIdea : function (){
+      var idea = {
+        'id':this.listaIdea.length,
+        'ideaTxt':this.ideaTxt,
+        'prioridad':this.prioridad
+      };
+      this.listaIdea.push(idea);
+    },
+    buscarIdeas: function (valor){
+      this.listaIdea = this.listaIdea.filter(value=>{return value.idea.indexOf(valor)>=0})
+    },
+    eliminarIdea : function (id){
+      let index = this.listaIdea.findIndex(e=>e.id===id);
+      this.listaIdea.splice(index,1);
+    },
+    cargarDatosUsuarios : function(){
+      var self = this;
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(
+         function(response){
+           self.listaUsuarios = response.data;
+           console.log(self.listaUsuarios)
+         }
+      )
+    }
+    },
+  watch: {
+    searchTxt: function (val){
+      this.buscarIdeas(val);
+    }
+  },
+
+})
